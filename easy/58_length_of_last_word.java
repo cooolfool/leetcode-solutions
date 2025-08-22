3813d2c8587619d63e8d4664ffd0d224ca09fe88
cooolfool <pratyushshandilya02@gmail.com>
2025-08-22 16:48:49 +0530

    refactor test

--- easy/58_length_of_last_word.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        String [] str =s.split(" ",0);
-        int len=str.length;
-        return str[len-1].length();
-
-        
-    }
-}
--- medium/75_sort_colors.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-            cout<<nums[i];
-    }
-};
